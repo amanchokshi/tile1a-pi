aae68c7f0f941fbc195a93bd33d18afe2f5866c1
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-09-10 20:31:46 +0800

    It worrksgit add -A

--- make_night_schedule.sh
+++ make_night_schedule.sh
@@ -5,8 +5,8 @@ OBS_LENGHT=1800
 START=`date +'%Y-%m-%d-%H:%M' -d '6am'`
 STOP=`date +'%Y-%m-%d-%H:%M' -d '6am +1 day'`
 
-# START=2019-09-10-13:30
-# STOP=2019-09-10-14:18
+# START=2019-09-10-14:40
+# STOP=2019-09-10-14:41
 # OBS_LENGHT=60
 
 
